e3b0c446cb873352ff2547bf2012e0b25a17d6d3
Carmine <carmine.schettino@fides.it>
2024-09-11 17:28:41 +0200

    +model example

--- apps/nx-next-angular/src/app/signal-component/signal-component.component.ts
+++ apps/nx-next-angular/src/app/signal-component/signal-component.component.ts
@@ -1,19 +1,26 @@
-import {Component, effect, input, OnInit, signal} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component, effect, input, model, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-signal-component',
   standalone: true,
   imports: [CommonModule],
+  changeDetection:ChangeDetectionStrategy.OnPush,
   templateUrl: './signal-component.component.html',
   styleUrl: './signal-component.component.css',
 })
 export class SignalComponentComponent  {
   quantity = signal<number>(1);
-  firstName = input.required<string>();         // InputSignal<string|undefined>
+  firstName = input.required<string>();
+  model = model.required<string>()
+
   constructor() {
+    setTimeout(() => {
+      this.model.set("mod child");
+    }, 3000)
     effect(() => {
-      console.log(this.firstName());
+      console.log('sign',this.firstName());
+      console.log('model',this.model());
     });
   }
 
